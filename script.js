3c0bce542470ff8124fbf7eb0314bc6b494d3548
Dk007ms <dkaushik290@gmail.com>
2023-11-06 18:07:10 +0530

    css changes

---
+++ script.js
@@ -0,0 +1,21 @@
+const wrapper=document.querySelector(".wrapper");
+const container=document.querySelector(".container");
+const title=document.querySelector(".title");
+const search_bar=document.querySelector(".search_bar");
+const details=document.querySelector(".details");
+const input=document.querySelector(".input");
+const photo =document.querySelector(".photo");
+const text_details =document.querySelector(".text_details");
+const detail1 =document.querySelector(".detail1");
+const name_joining =document.querySelector(".name_joining");
+const name =document.querySelector(".name");
+const join_date =document.querySelector(".join_date");
+const username =document.querySelector(".username");
+const description =document.querySelector(".description");
+const github_repos =document.querySelector(".github_repos");
+const Repos =document.querySelector(".Repos");
+const Followers =document.querySelector(".Followers");
+const Following =document.querySelector(".Following");
+const links =document.querySelector(".links");
+
+
